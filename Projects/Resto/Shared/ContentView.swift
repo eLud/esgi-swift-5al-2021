f78d64a828e91b18e036d30222003c5ca81669ed
Ludovic Ollagnier <ludovic@tectec.fr>
2021-11-19 11:41:54 +0100

    Updated example

--- Projects/Resto/Shared/ContentView.swift
+++ Projects/Resto/Shared/ContentView.swift
@@ -12,11 +12,53 @@ struct ContentView: View {
     @Binding var listeResto: [Resto]
     @State private var search: String = ""
 
+    @AppStorage("lastName") var lastName: String = "No lastName"
+
     var body: some View {
         List() {
             Text("Resto 1")
+            Section {
+                Text(readInUserDefaults())
+                Button("Save firstName") {
+                    persistInUserDefaults()
+                }
+            }
+            Section {
+                Text(lastName)
+                Button("Save lastName") {
+                    lastName = "Ollagnier"
+                }
+            }
+
+            Section {
+                Button("Save Restos") {
+                    saveJSON(from: listeResto)
+                }
+            }
         }
         .searchable(text: $search)
+    }
+
+    func persistInUserDefaults() {
+        let def = UserDefaults.standard
+        def.set("Ludovic", forKey: "firstName")
+    }
+
+    func readInUserDefaults() -> String {
+        let def = UserDefaults.standard
+        let inDefaults = def.string(forKey: "firstName")
+        return inDefaults ?? "No firstName"
+    }
+
+    func saveJSON(from data : [Resto]) {
+        let encoder = JSONEncoder()
+        let data = try? encoder.encode(data)
+
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let fullUrl = url.appendingPathComponent("backup.json")
+
+        try? data?.write(to: fullUrl)
+        // let data = Data(contentsOf: fullURL)
     }
 }
 
--- Projects/Resto/Shared/Rsto.swift
+++ Projects/Resto/Shared/Rsto.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
-struct Resto {
+struct Resto: Codable {
     var name: String
+    var note: Float
+
+    var isOpen: Bool
 }
 
-struct Annuaire {
+struct Annuaire: Codable {
     var restos: [Resto] = []
+
+    init() {
+        let resto1 = Resto(name: "Resto1", note: 5, isOpen: true)
+        let resto2 = Resto(name: "Resto2", note: 8, isOpen: false)
+        let resto3 = Resto(name: "Resto3", note: 2, isOpen: true)
+        restos = [resto1, resto2, resto3]
+    }
 }
